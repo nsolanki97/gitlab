002480da61c499641a9691bc4b3a6cee94077335
Nitirajsinh Solanki <solankin@go.stockton.edu>
2020-03-31 17:19:03 -0400

    Added a default value

--- Hello.java
+++ Hello.java
@@ -1,5 +1,10 @@
 public class Hello {
     public static void main(String argv[]){
-        System.out.println("Hello World");
+        String name = "World";
+        if(argv.length != 0)
+        {
+            name = argv[0];
+        }
+        System.out.println("Hello, "+ name + "!");
     }
 }
